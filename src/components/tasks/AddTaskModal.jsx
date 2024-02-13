a129aaa3232ad99565d57783f2248a2afb36b056
Shohidul Arifin <shohidularifin@gmail.com>
2024-02-13 03:19:21 -0800

    react hook form and tailwind form plugin used in AddTaskModal

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -1,14 +1,97 @@
+import { useForm } from "react-hook-form";
 import Modal from "../ui/Modal";
 
 // eslint-disable-next-line react/prop-types
 const AddTaskModal = ({ isOpen, setIsOpen }) => {
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    onCancel();
+  };
+
+  const onCancel = () => {
+    reset();
+    setIsOpen(false);
+  };
   return (
     <div>
       <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="qtec project">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
-          cumque.
-        </p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col mb-5">
+            <label htmlFor="title" className="mb-2">
+              Title
+            </label>
+            <input
+              className="w-full rounded-md"
+              type="text"
+              id="title"
+              {...register("title")}
+            />
+          </div>
+          <div className="flex flex-col mb-5">
+            <label htmlFor="title" className="mb-2">
+              Description
+            </label>
+            <input
+              className="w-full rounded-md"
+              type="text"
+              id="description"
+              {...register("description")}
+            />
+          </div>
+          <div className="flex flex-col mb-5">
+            <label htmlFor="title" className="mb-2">
+              Deadline
+            </label>
+            <input
+              className="w-full rounded-md"
+              type="date"
+              id="date"
+              {...register("date")}
+            />
+          </div>
+          <div className="flex flex-col mb-5">
+            <label htmlFor="title" className="mb-2">
+              Assign to
+            </label>
+            <select
+              className="w-full rounded-md"
+              id="assignedTo"
+              {...register("assignedTo")}
+            >
+              <option value="Shohidul Arfin">Shohidul Arifin</option>
+              <option value="Kamal Khan">Kamal Khan</option>
+            </select>
+          </div>
+          <div className="flex flex-col mb-5">
+            <label htmlFor="title" className="mb-2">
+              Priority
+            </label>
+            <select
+              className="w-full rounded-md"
+              id="priority"
+              {...register("priority")}
+            >
+              <option defaultValue value="high">
+                High
+              </option>
+              <option value="medium">Medium</option>
+              <option value="low">Low</option>
+            </select>
+          </div>
+          <div className="flex gap-3 justify-end">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+            <button
+              onClick={() => onCancel()}
+              type="button"
+              className="btn btn-danger"
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
       </Modal>
     </div>
   );
